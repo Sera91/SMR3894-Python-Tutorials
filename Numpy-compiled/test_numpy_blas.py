d3aab80866aa434f289bc3381f9a8eb3a8cd6d97
Sera91 <serafina.digioia@inaf.it>
2023-11-07 13:48:07 +0100

    new file:   test_numpy_blas.py

---
+++ Numpy-compiled/test_numpy_blas.py
@@ -0,0 +1,10 @@
+import numpy
+from numpy.distutils.system_info import get_info
+import sys
+import timeit
+
+
+print("Numpy version: %s" % numpy.__version__)
+
+info = get_info('blas_opt')
+print("blas_version: ", info)
