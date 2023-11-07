b7d72c6f9ad6710d4639cc3996b445d7d3f07258
Sera91 <serafina.digioia@inaf.it>
2023-11-08 00:51:10 +0100

    deleted:    Numpy-compiled/Installing_OpenBLAS.md

--- Numpy-compiled/check-py-dev.py
+++
@@ -1,4 +0,0 @@
-from distutils.sysconfig import get_makefile_filename as m
-from os.path import isfile 
-import sys 
-sys.exit(not isfile(m()))
--- Numpy-compiled/test-Numpy/test_for_loop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# declare an array called array and define 3 vales
-array=( one two three )
-for i in "${array[@]}"
-do
-	echo "$i"
-done
