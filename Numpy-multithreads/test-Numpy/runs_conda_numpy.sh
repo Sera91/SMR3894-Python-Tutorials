63eb9419afd79a19332a64bd2e8d3245d32643ca
Serafina Di Gioia <eirene1991@gmail.com>
2023-11-09 11:38:58 +0100

    Update runs_conda_numpy.sh

--- Numpy-multithreads/test-Numpy/runs_conda_numpy.sh
+++ Numpy-multithreads/test-Numpy/runs_conda_numpy.sh
@@ -12,13 +12,13 @@ for i in "${array[@]}"
 do
  OMP_NUM_THREADS=1
  echo "N_threads=1"
- OPENBLAS_NUM_THREADS=1 python test_numpy_dot3.py $i
+ OMP_NUM_THREADS=1 python test_numpy_dot3.py $i
  echo "N_threads=2"
- OPENBLAS_NUM_THREADS=2 python test_numpy_dot3.py $i
+ OMP_NUM_THREADS=2 python test_numpy_dot3.py $i
  echo "N_threads=4"
- OPENBLAS_NUM_THREADS=4 python test_numpy_dot3.py $i
+ OMP_NUM_THREADS=4 python test_numpy_dot3.py $i
  echo "N_threads=8"
- OPENBLAS_NUM_THREADS=8 python test_numpy_dot3.py $i
+ OMP_NUM_THREADS=8 python test_numpy_dot3.py $i
 done
 
 deactivate
